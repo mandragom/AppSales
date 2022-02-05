0bd7cb32bc806d5abb22c3728c6c8c81678aa686
Omar <mandragom@gmail.com>
2022-02-04 22:06:23 -0600

    Relationship

---
+++ Sales.Backend/Models/LocalDataContext.cs
@@ -0,0 +1,13 @@
+﻿using DomainSales.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackendSales.Models
+{
+    public class LocalDataContext : DataContext
+    {
+        public System.Data.Entity.DbSet<CommonSales.Models.Product> Products { get; set; }
+    }
+}
